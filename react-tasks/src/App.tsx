80ded01d0adaa907ea3a578944a4dfb779e337bb
m208 <>
2022-10-05 17:30:59 +0300

    feat: add routing

--- react-tasks/src/App.tsx
+++ react-tasks/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from 'components/MainPage/MainPage';
+import About from 'components/About/About';
+import Page404 from 'components/Page404/Page404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/404" element={<Page404 />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ react-tasks/src/components/Page404/Page404.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page404() {
+  return (
+    <div>
+      <p>Page does not exist</p>
+    </div>
+  );
+}
