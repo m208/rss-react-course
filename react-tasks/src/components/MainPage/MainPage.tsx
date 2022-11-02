f5bf66e23faf2cb792407e494c5440d44ce845be
m208 <avcvet@yandex.ru>
2022-11-02 22:54:28 +0300

    refactor: mainPage content

--- react-tasks/src/components/MainPage/MainPage.tsx
+++ react-tasks/src/components/MainPage/MainPage.tsx
@@ -1,21 +1,29 @@
+import Card from 'components/Card/Card';
+import SearchBar from 'components/SearchBar/SearchBar';
 import React from 'react';
+import './MainPage.css';
 
 export default function MainPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <SearchBar />
+
+        <div className="cards-wrapper">
+          <Card
+            imgSrc="https://placeimg.com/320/320/animals"
+            header="Header"
+            description={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
+            date="01.01.2022"
+          />
+          <Card
+            imgSrc="https://placeimg.com/320/320/tech"
+            header="Another Header"
+            description={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
+            date="02.02.2022"
+          />
+        </div>
+      </div>
+    </>
   );
 }
