1e4f851f014cf7e66e0bac74b897bdbe447240e3
m208 <avcvet@yandex.ru>
2022-11-02 22:37:04 +0300

    feat: fill card content via props

--- react-tasks/src/components/Card/Card.tsx
+++ react-tasks/src/components/Card/Card.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './Card.css';
 
-export default function Card() {
+interface CardContentProps {
+  imgSrc: string;
+  header: string;
+  description: string;
+  date: string;
+}
+
+export default function Card({ imgSrc, header, description, date }: CardContentProps) {
   return (
     <div className="card-wrapper">
       <div className="card-image">
-        <img src="https://placeimg.com/320/320/any" alt="Picture" />
+        <img src={imgSrc} alt="Picture" />
       </div>
       <div className="card-heading">
-        <h2 className="card-heading-content">Header</h2>
+        <h2 className="card-heading-content">{header}</h2>
       </div>
       <div className="card-description">
-        <p className="card-description-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga cumque ipsum similique est
-          officia sunt error illum ipsa amet voluptatibus!
-        </p>
+        <p className="card-description-content">{description}</p>
       </div>
       <div className="card-date">
-        <p className="card-date-content">01.01.2022</p>
+        <p className="card-date-content">{date}</p>
       </div>
       <div className="card-button-wrapper">
         <div className="card-button">More...</div>
