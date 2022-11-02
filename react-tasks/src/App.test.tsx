1bc37d7867401e24269ed61e6a4c4b0203903284
m208 <avcvet@yandex.ru>
2022-11-03 01:23:28 +0300

    feat: add tests, mock local storage

--- react-tasks/src/App.test.tsx
+++ react-tasks/src/App.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import fakeLocalStorage from './LSMock';
+
+Object.defineProperty(window, 'localStorage', {
+  value: fakeLocalStorage,
+});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/undefined/i);
+//   expect(linkElement).toBeInTheDocument();
+
+// });
+
+describe('SearchBar', () => {
+  it('get it initial value from local storage', () => {
+    localStorage.setItem('searchBarSavedValue', 'TEST_VALUE');
+    render(<App />);
+    const searchInput = screen.getByPlaceholderText<HTMLInputElement>('Search...');
+    expect(searchInput).toBeInTheDocument();
+    expect(searchInput.value).toBe('TEST_VALUE');
+  });
 });
---
+++ react-tasks/src/LSMock.ts
@@ -0,0 +1,18 @@
+export default (function () {
+  let store: Record<string, string> = {};
+
+  return {
+    getItem: function (key: string) {
+      return store[key] || null;
+    },
+    setItem: function (key: string, value: string) {
+      store[key] = value.toString();
+    },
+    removeItem: function (key: string) {
+      delete store[key];
+    },
+    clear: function () {
+      store = {};
+    },
+  };
+})();
