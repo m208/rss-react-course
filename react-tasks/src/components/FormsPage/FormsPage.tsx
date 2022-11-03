ca982ba1c151e988682ae6dc0cbb531f43297f63
m208 <avcvet@yandex.ru>
2022-11-03 23:05:02 +0300

    feat: add submit logic

--- react-tasks/src/components/FormsPage/FormsPage.tsx
+++ react-tasks/src/components/FormsPage/FormsPage.tsx
@@ -1,7 +1,22 @@
+import Card from 'components/Card/Card';
 import React, { FormEvent, RefObject } from 'react';
 import './FormsPage.css';
 
-export class FormsPage extends React.Component {
+interface FormData {
+  name: string;
+  email: string;
+  country: string;
+  date: string;
+  file: string;
+  cb1: boolean;
+  cb2: boolean;
+  switcher: boolean;
+}
+
+export class FormsPage extends React.Component<
+  Record<string, unknown>,
+  { cards: Array<FormData> }
+> {
   name: RefObject<HTMLInputElement>;
   email: RefObject<HTMLInputElement>;
   country: RefObject<HTMLSelectElement>;
@@ -13,112 +28,153 @@ export class FormsPage extends React.Component {
 
   constructor(props: Record<string, string>) {
     super(props);
+    this.state = { cards: [] };
     this.handleSubmit = this.handleSubmit.bind(this);
+
     this.name = React.createRef();
     this.email = React.createRef();
     this.country = React.createRef();
+    this.date = React.createRef();
+    this.file = React.createRef();
     this.cb1 = React.createRef();
     this.cb2 = React.createRef();
     this.switcher = React.createRef();
-    this.file = React.createRef();
-    this.date = React.createRef();
   }
 
   handleSubmit(event: FormEvent<HTMLFormElement>) {
-    console.log('A name was submitted: ' + this.name.current!.value);
+    console.log(this.cb1.current!.checked);
+
+    const data: FormData = {
+      name: this.name.current!.value,
+      email: this.email.current!.value,
+      country: this.country.current!.value,
+      date: this.date.current!.value,
+      file: this.file.current!.value,
+      cb1: this.cb1.current!.checked,
+      cb2: this.cb2.current!.checked,
+      switcher: this.switcher.current!.checked,
+    };
+
+    const cardsCopy = [...this.state.cards];
+    cardsCopy.push(data);
+    this.setState({ cards: cardsCopy });
+
     event.preventDefault();
   }
 
   render() {
     return (
-      <div className="form-wrapper">
-        <form className="form-example" onSubmit={(e) => this.handleSubmit(e)}>
-          <div className="form-item">
-            <label className="form-item-label">
-              Enter your name:
-              <input type="text" name="name" id="name" required ref={this.name} />
-            </label>
-          </div>
+      <>
+        <div className="form-wrapper">
+          <form className="form-example" onSubmit={(e) => this.handleSubmit(e)}>
+            <div className="form-item">
+              <label className="form-item-label">
+                Enter your name:
+                <input type="text" name="name" id="name" required ref={this.name} />
+              </label>
+            </div>
 
-          <div className="form-item">
-            <label className="form-item-label">
-              Enter your email:
-              <input type="email" name="email" id="email" ref={this.email} />
-            </label>
-          </div>
+            <div className="form-item">
+              <label className="form-item-label">
+                Enter your email:
+                <input type="email" name="email" id="email" ref={this.email} />
+              </label>
+            </div>
 
-          <div className="form-item">
-            <label className="form-item-label">
-              Select country
-              <select ref={this.country}>
-                <option selected value="norway">
-                  Norway
-                </option>
-                <option value="finland">Finland</option>
-                <option value="sweden">Sweden</option>
-                <option value="denmark">Denmark</option>
-              </select>
-            </label>
-          </div>
+            <div className="form-item">
+              <label className="form-item-label">
+                Select country
+                <select ref={this.country}>
+                  <option defaultValue="Norway">Norway</option>
+                  <option value="Finland">Finland</option>
+                  <option value="Sweden">Sweden</option>
+                  <option value="Denmark">Denmark</option>
+                </select>
+              </label>
+            </div>
 
-          <div className="form-item">
-            <label className="form-item-label">
-              Select date
-              <input
-                type="date"
-                value="2022.01.11"
-                min="2018-01-01"
-                max="2023-12-31"
-                ref={this.date}
-              />
-            </label>
-          </div>
-
-          <div className="form-item">
-            <label className="form-item-label">
-              Upload file
-              <input type="file" ref={this.file} />
-            </label>
-          </div>
-
-          <div className="form-item">
-            <label className="form-item-label">
-              I consent to my personal data:
-              <input type="checkbox" ref={this.cb1} />
-            </label>
-          </div>
-
-          <div className="form-item">
-            <label className="form-item-label">
-              I want to receive notifications
-              <input type="checkbox" ref={this.cb2} />
-            </label>
-          </div>
-
-          <div className="form-item">
-            <label className="form-item-label">
-              Switcher
-              <div className="toggle-switch">
+            <div className="form-item">
+              <label className="form-item-label">
+                Select date
                 <input
-                  type="checkbox"
-                  className="toggle-switch-checkbox"
-                  name="toggleSwitch"
-                  id="toggleSwitch"
-                  ref={this.switcher}
+                  type="date"
+                  defaultValue="2022-01-11"
+                  min="2018-01-01"
+                  max="2023-12-31"
+                  ref={this.date}
                 />
-                <label className="toggle-switch-label" htmlFor="toggleSwitch">
-                  <span className="toggle-switch-inner" />
-                  <span className="toggle-switch-switch" />
-                </label>
-              </div>
-            </label>
-          </div>
+              </label>
+            </div>
 
-          <div className="form-item">
-            <input type="submit" value="Subscribe!" />
-          </div>
-        </form>
-      </div>
+            <div className="form-item">
+              <label className="form-item-label">
+                Upload file
+                <input type="file" ref={this.file} />
+              </label>
+            </div>
+
+            <div className="form-item">
+              <label className="form-item-label">
+                I consent to my personal data:
+                <input type="checkbox" ref={this.cb1} />
+              </label>
+            </div>
+
+            <div className="form-item">
+              <label className="form-item-label">
+                I want to receive notifications
+                <input type="checkbox" ref={this.cb2} />
+              </label>
+            </div>
+
+            <div className="form-item">
+              <label className="form-item-label">
+                Switcher
+                <div className="toggle-switch">
+                  <input
+                    type="checkbox"
+                    className="toggle-switch-checkbox"
+                    name="toggleSwitch"
+                    id="toggleSwitch"
+                    ref={this.switcher}
+                  />
+                  <label className="toggle-switch-label" htmlFor="toggleSwitch">
+                    <span className="toggle-switch-inner" />
+                    <span className="toggle-switch-switch" />
+                  </label>
+                </div>
+              </label>
+            </div>
+
+            <div className="form-item">
+              <input type="submit" value="Subscribe!" />
+            </div>
+          </form>
+        </div>
+
+        <div className="cards-wrapper">
+          {this.state.cards.map((el, i) => (
+            <Card
+              imgSrc="https://placeimg.com/320/320/tech"
+              date={el.date}
+              header={el.name}
+              description={generateText(el)}
+              key={i}
+            />
+          ))}
+        </div>
+      </>
     );
   }
+}
+
+function generateText(item: FormData) {
+  return `
+    email: ${item.email}\n
+    country: ${item.country}\n
+    file name: ${item.file}\n
+    checkbox 1: ${item.cb1 ? 'yes' : 'no'}\n
+    checkbox 2: ${item.cb2 ? 'yes' : 'no'}\n
+    switcher: ${item.switcher ? 'yes' : 'no'}\n
+    `;
 }
