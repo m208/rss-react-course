00fa5ea4d81700bea17d63f894002fe03051c278
m208 <avcvet@yandex.ru>
2022-11-06 20:55:49 +0300

    refactor: NotificationPopup as functional component

--- react-tasks/src/components/FormsPage/NotificationPopUp/NotificationPopUp.tsx
+++ react-tasks/src/components/FormsPage/NotificationPopUp/NotificationPopUp.tsx
@@ -5,25 +5,12 @@ interface NotificationPopUpProps {
   message: string;
 }
 
-interface NotificationPopUpState {
-  active: boolean;
-}
-
-export class NotificationPopUp extends React.Component<
-  NotificationPopUpProps,
-  NotificationPopUpState
-> {
-  constructor(props: NotificationPopUpProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="popup-background">
-        <div className="popup-inner">
-          <h3 className="popup-text">{this.props.message}</h3>
-        </div>
+export function NotificationPopUp({ message }: NotificationPopUpProps) {
+  return (
+    <div className="popup-background">
+      <div className="popup-inner">
+        <h3 className="popup-text">{message}</h3>
       </div>
-    );
-  }
+    </div>
+  );
 }
