f46bc0920f05eea059c90ef2f9be8bd1251d4c09
m208 <avcvet@yandex.ru>
2022-11-06 20:53:16 +0300

    refactor: FormsPage with react-hook-form

--- react-tasks/src/components/FormsPage/FormsPage.tsx
+++ react-tasks/src/components/FormsPage/FormsPage.tsx
@@ -1,5 +1,6 @@
 import Card from 'components/Card/Card';
-import React, { ChangeEvent, FormEvent, RefObject } from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 import './FormsPage.css';
 import { NotificationPopUp } from './NotificationPopUp/NotificationPopUp';
 
@@ -8,238 +9,206 @@ interface FormData {
   email: string;
   country: string;
   date: string;
-  file: string;
+  file?: string;
   fileData: string | null;
   cb1: boolean;
   cb2: boolean;
   switcher: boolean;
 }
 
-interface FormState {
-  cards: Array<FormData>;
-  submitActive: boolean;
-  popupActive: boolean;
-}
+export function FormsPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-export class FormsPage extends React.Component<Record<string, unknown>, FormState> {
-  name: RefObject<HTMLInputElement>;
-  email: RefObject<HTMLInputElement>;
-  country: RefObject<HTMLSelectElement>;
-  cb1: RefObject<HTMLInputElement>;
-  cb2: RefObject<HTMLInputElement>;
-  switcher: RefObject<HTMLInputElement>;
-  file: RefObject<HTMLInputElement>;
-  date: RefObject<HTMLInputElement>;
-  form: RefObject<HTMLFormElement>;
-  fileReader: FileReader;
-  fileData: string | null;
+  const [submitActive, setsubmitActive] = useState(false);
+  const [popupActive, setpopupActive] = useState(false);
+  const [cards, setCards] = useState<Array<FormData>>([]);
 
-  constructor(props: Record<string, string>) {
-    super(props);
-    this.state = { cards: [], submitActive: false, popupActive: false };
-    this.handleSubmit = this.handleSubmit.bind(this);
+  const imageData = useRef<string>('');
 
-    this.name = React.createRef();
-    this.email = React.createRef();
-    this.country = React.createRef();
-    this.date = React.createRef();
-    this.file = React.createRef();
-    this.cb1 = React.createRef();
-    this.cb2 = React.createRef();
-    this.switcher = React.createRef();
-
-    this.form = React.createRef();
-
-    this.fileReader = new FileReader();
-    this.fileData = null;
-  }
-
-  handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const data: FormData = {
-      name: this.name.current!.value,
-      email: this.email.current!.value,
-      country: this.country.current!.value,
-      date: this.date.current!.value,
-      file: this.file.current!.value,
-      fileData: this.fileData,
-      cb1: this.cb1.current!.checked,
-      cb2: this.cb2.current!.checked,
-      switcher: this.switcher.current!.checked,
+  const onSubmit = async (data: FieldValues) => {
+    const { name, email, country, date, cb1, cb2, switcher } = data;
+    const card: FormData = {
+      name,
+      email,
+      country,
+      date,
+      cb1,
+      cb2,
+      switcher,
+      fileData: imageData.current,
     };
+    setCards((cards) => [...cards, card]);
 
-    const cardsCopy = [...this.state.cards];
-    cardsCopy.push(data);
-    this.setState({ cards: cardsCopy });
+    drawPopUp();
+    reset();
+    imageData.current = '';
+  };
 
-    this.setState({ popupActive: true });
-    setTimeout(() => this.setState({ popupActive: false }), 1500);
+  const drawPopUp = () => {
+    setpopupActive(true);
+    setTimeout(() => setpopupActive(false), 1500);
+  };
 
-    this.formReset();
-  }
+  const fileReader = new FileReader();
 
-  onValidationInputChange() {
-    this.setState({ submitActive: true });
-  }
-
-  onValidationFailed() {
-    this.setState({ submitActive: false });
-  }
-
-  formReset() {
-    this.form.current!.reset();
-    this.setState({ submitActive: false });
-    this.fileData = null;
-  }
-
-  onFileUpload(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ submitActive: true });
+  const onFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    setsubmitActive(true);
 
     if (event.target) {
       const files = (event.target as HTMLInputElement).files;
       if (files && files.length) {
-        this.fileReader.onload = () => {
-          this.fileData = this.fileReader.result as string;
-          console.log(this.fileData);
+        fileReader.onload = () => {
+          imageData.current = fileReader.result as string;
         };
-        this.fileReader.readAsDataURL(files[0]);
+        fileReader.readAsDataURL(files[0]);
       }
     }
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <div className="form-wrapper">
-          <form
-            className="form-example"
-            onSubmit={(e) => this.handleSubmit(e)}
-            onInvalid={() => this.onValidationFailed()}
-            ref={this.form}
-          >
-            <div className="form-item">
-              <label className="form-item-label">
-                Enter your name:
+  const onValidationInputChange = () => {
+    setsubmitActive(true);
+  };
+
+  return (
+    <>
+      <div className="form-wrapper">
+        <form className="form-example" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-item">
+            <label className="form-item-label">
+              Enter your name:
+              <div className="form-input">
                 <input
+                  {...register('name', { required: true })}
+                  aria-invalid={errors.name ? 'true' : 'false'}
                   type="text"
-                  name="name"
-                  id="name"
-                  required
-                  ref={this.name}
-                  onChange={() => this.onValidationInputChange()}
+                  onChange={() => onValidationInputChange()}
                 />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                Enter your email:
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  required
-                  ref={this.email}
-                  onChange={() => this.onValidationInputChange()}
-                />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                Select country
-                <select ref={this.country}>
-                  <option defaultValue="Norway">Norway</option>
-                  <option value="Finland">Finland</option>
-                  <option value="Sweden">Sweden</option>
-                  <option value="Denmark">Denmark</option>
-                </select>
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                Select date
-                <input
-                  type="date"
-                  defaultValue="2022-01-11"
-                  min="2018-01-01"
-                  max="2023-12-31"
-                  ref={this.date}
-                />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                Upload image
-                <input
-                  type="file"
-                  ref={this.file}
-                  onChange={(e) => this.onFileUpload(e)}
-                  accept="image/*"
-                />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                I consent to my personal data:
-                <input type="checkbox" ref={this.cb1} />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                I want to receive notifications
-                <input type="checkbox" ref={this.cb2} />
-              </label>
-            </div>
-
-            <div className="form-item">
-              <label className="form-item-label">
-                Switcher
-                <div className="toggle-switch">
-                  <input
-                    type="checkbox"
-                    className="toggle-switch-checkbox"
-                    name="toggleSwitch"
-                    id="toggleSwitch"
-                    ref={this.switcher}
-                  />
-                  <label className="toggle-switch-label" htmlFor="toggleSwitch">
-                    <span className="toggle-switch-inner" />
-                    <span className="toggle-switch-switch" />
-                  </label>
+                <div className="form-error-field">
+                  {errors.name?.type === 'required' && (
+                    <span role="alert">First name is required</span>
+                  )}
                 </div>
-              </label>
-            </div>
+              </div>
+            </label>
+          </div>
 
-            <div className="form-item">
-              <input type="submit" value="Subscribe!" disabled={!this.state.submitActive} />
-            </div>
-          </form>
-        </div>
+          <div className="form-item">
+            <label className="form-item-label">
+              Enter your email:
+              <div className="form-input">
+                <input
+                  {...register('email', {
+                    required: 'Email Address is required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: 'invalid email address',
+                    },
+                  })}
+                  aria-invalid={errors.email ? 'true' : 'false'}
+                  onChange={() => onValidationInputChange()}
+                />
+                <div className="form-error-field">
+                  {errors.email && <span role="alert">{errors.email?.message?.toString()}</span>}
+                </div>
+              </div>
+            </label>
+          </div>
 
-        {this.state.popupActive && (
-          <NotificationPopUp message="Success! Information has been saved!" />
-        )}
+          <div className="form-item">
+            <label className="form-item-label">
+              Select country
+              <select {...register('country')}>
+                <option defaultValue="Norway">Norway</option>
+                <option value="Finland">Finland</option>
+                <option value="Sweden">Sweden</option>
+                <option value="Denmark">Denmark</option>
+              </select>
+            </label>
+          </div>
 
-        <div className="cards-wrapper">
-          {this.state.cards.map((el, i) => (
-            <Card
-              imgSrc={el.fileData}
-              date={el.date}
-              header={el.name}
-              description={generateText(el)}
-              key={i}
-            />
-          ))}
-        </div>
-      </>
-    );
-  }
+          <div className="form-item">
+            <label className="form-item-label">
+              Select date
+              <input
+                {...register('date')}
+                type="date"
+                defaultValue="2022-01-11"
+                min="2018-01-01"
+                max="2023-12-31"
+              />
+            </label>
+          </div>
+
+          <div className="form-item">
+            <label className="form-item-label">
+              Upload image
+              <input
+                {...register('file')}
+                type="file"
+                onChange={(e) => onFileUpload(e)}
+                accept="image/*"
+              />
+            </label>
+          </div>
+
+          <div className="form-item">
+            <label className="form-item-label">
+              I consent to my personal data:
+              <input type="checkbox" {...register('cb1')} />
+            </label>
+          </div>
+
+          <div className="form-item">
+            <label className="form-item-label">
+              I want to receive notifications
+              <input type="checkbox" {...register('cb2')} />
+            </label>
+          </div>
+
+          <div className="form-item">
+            <label className="form-item-label">
+              Switcher
+              <div className="toggle-switch">
+                <input
+                  type="checkbox"
+                  className="toggle-switch-checkbox"
+                  id="toggleSwitch"
+                  {...register('switcher')}
+                />
+                <label className="toggle-switch-label" htmlFor="toggleSwitch">
+                  <span className="toggle-switch-inner" />
+                  <span className="toggle-switch-switch" />
+                </label>
+              </div>
+            </label>
+          </div>
+
+          <div className="form-item">
+            <input type="submit" value="Subscribe!" disabled={!submitActive} />
+          </div>
+        </form>
+      </div>
+
+      {popupActive && <NotificationPopUp message="Success! Information has been saved!" />}
+
+      <div className="cards-wrapper">
+        {cards.map((el, i) => (
+          <Card
+            imgSrc={el.fileData}
+            date={el.date}
+            header={el.name}
+            description={generateText(el)}
+            key={i}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
 function generateText(item: FormData) {
