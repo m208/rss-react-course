e5f600fa52da88084414f06d147008b9b4541dd2
m208 <avcvet@yandex.ru>
2022-11-06 16:48:01 +0300

    refactor: MainPage with hooks as function

--- react-tasks/src/components/MainPage/MainPage.tsx
+++ react-tasks/src/components/MainPage/MainPage.tsx
@@ -1,93 +1,64 @@
 import { generateImageUrl, generatePostDate } from 'api/flickr';
-import { FlickrSearchItem, FlickrSearchResult } from 'api/types';
+import { FlickrSearchItem } from 'api/types';
 import Card from 'components/Card/Card';
 import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.css';
 
-interface MainPageProps {
-  a?: string;
-}
+export function MainPage() {
+  const [searchResults, setSearchResults] = useState<Array<FlickrSearchItem>>([]);
+  const [spinnerActive, setSpinnerActive] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
+  const [modalContent, setModalContent] = useState<FlickrSearchItem | null>(null);
 
-interface MainPageState {
-  searchResults: Array<FlickrSearchItem>;
-  spinnerActive: boolean;
-  modalActive: boolean;
-  modalContent: FlickrSearchItem | null;
-}
-
-export class MainPage extends React.Component<MainPageProps, MainPageState> {
-  constructor(props: MainPageProps) {
-    super(props);
-
-    this.state = {
-      searchResults: [],
-      spinnerActive: false,
-      modalActive: false,
-      modalContent: null,
-    };
-  }
-
-  searchBarCallback = (searchResult: FlickrSearchItem) => {
-    this.setState({
-      searchResults: this.state.searchResults.concat(searchResult),
-    });
+  const searchBarCallback = (searchResult: FlickrSearchItem) => {
+    setSearchResults((searchResults) => [...searchResults, searchResult]);
   };
 
-  ajaxAnimationCallback = (status: boolean) => {
-    this.setState({
-      spinnerActive: status,
-    });
+  const ajaxAnimationCallback = (status: boolean) => {
+    setSpinnerActive(status);
   };
 
-  modalShowCallback = (status: boolean) => {
-    this.setState({
-      modalActive: status,
-    });
+  const modalShowCallback = (status: boolean) => {
+    setModalActive(status);
   };
 
-  openModal(index: number) {
-    if (this.state.searchResults[index]) {
-      this.modalShowCallback(true);
-      this.setState({
-        modalContent: this.state.searchResults[index],
-      });
+  const openModal = (index: number) => {
+    if (searchResults[index]) {
+      modalShowCallback(true);
+      setModalContent(searchResults[index]);
     }
-  }
+  };
 
-  render() {
-    return (
-      <>
-        <div className="App">
-          {this.state.spinnerActive && <Loader />}
+  return (
+    <>
+      <div className="App">
+        {spinnerActive && <Loader />}
 
-          {this.state.modalActive && (
-            <Modal content={this.state.modalContent} onCloseCallback={this.modalShowCallback} />
-          )}
+        {modalActive && <Modal content={modalContent} onCloseCallback={modalShowCallback} />}
 
-          <SearchBar
-            searchCallBack={this.searchBarCallback}
-            ajaxAnimationCallback={this.ajaxAnimationCallback}
-          />
+        <SearchBar
+          searchCallBack={searchBarCallback}
+          ajaxAnimationCallback={ajaxAnimationCallback}
+        />
 
-          <div className="cards-wrapper">
-            {this.state.searchResults.map((el, index) => (
-              <Card
-                cardType="mini"
-                imgSrc={generateImageUrl(el, 'n')}
-                date={generatePostDate(el)}
-                header={el.title}
-                description={el.description._content}
-                key={el.id}
-                onButtonClick={() => this.openModal(index)}
-              />
-            ))}
-          </div>
+        <div className="cards-wrapper">
+          {searchResults.map((el, index) => (
+            <Card
+              cardType="mini"
+              imgSrc={generateImageUrl(el, 'n')}
+              date={generatePostDate(el)}
+              header={el.title}
+              description={el.description._content}
+              key={el.id}
+              onButtonClick={() => openModal(index)}
+            />
+          ))}
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
