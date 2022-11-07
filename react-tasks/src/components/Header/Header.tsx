43c90ced29253dbdad3dd5b91a48db866f069757
m208 <avcvet@yandex.ru>
2022-11-07 20:39:17 +0300

    fix: add Links in Header

--- react-tasks/src/components/Header/Header.tsx
+++ react-tasks/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 interface HeaderProps {
@@ -9,6 +10,10 @@ export default function Header({ currentPage }: HeaderProps) {
   return (
     <header className="header-wrapper">
       <p className="header-current-page">{currentPage}</p>
+      <div className="header-links"></div>
+      <Link to="/">MAIN</Link>
+      <Link to="/forms">FORMS</Link>
+      <Link to="/about">ABOUT</Link>
     </header>
   );
 }
