1f1e6df99c1c705ae51ce19ecd836c0c2aa9661a
m208 <avcvet@yandex.ru>
2022-11-10 01:04:03 +0300

    refactor: remove previous context

--- react-tasks/src/App.tsx
+++ react-tasks/src/App.tsx
@@ -6,55 +6,49 @@ import About from 'components/About/About';
 import Page404 from 'components/Page404/Page404';
 import Header from 'components/Header/Header';
 import { FormsPage } from 'components/FormsPage/FormsPage';
-import { SearchState } from 'context/SearchContext';
-import { FormsState } from 'context/FormsContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <SearchState>
-        <FormsState>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Header currentPage="Main Page" />
-                  <MainPage />
-                </>
-              }
-            />
-            <Route
-              path="/forms"
-              element={
-                <>
-                  <Header currentPage="Forms Page" />
-                  <FormsPage />
-                </>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <>
-                  <Header currentPage="About Page" />
-                  <About />
-                </>
-              }
-            />
-            <Route
-              path="/404"
-              element={
-                <>
-                  <Header currentPage="404 Page" />
-                  <Page404 />
-                </>
-              }
-            />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-          </Routes>
-        </FormsState>
-      </SearchState>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header currentPage="Main Page" />
+              <MainPage />
+            </>
+          }
+        />
+        <Route
+          path="/forms"
+          element={
+            <>
+              <Header currentPage="Forms Page" />
+              <FormsPage />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header currentPage="About Page" />
+              <About />
+            </>
+          }
+        />
+        <Route
+          path="/404"
+          element={
+            <>
+              <Header currentPage="404 Page" />
+              <Page404 />
+            </>
+          }
+        />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
