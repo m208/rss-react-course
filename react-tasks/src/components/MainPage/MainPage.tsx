380a50532ca1a11fd055d537861e137f0f995aa5
m208 <avcvet@yandex.ru>
2022-11-10 00:36:10 +0300

    refactor: MainPage using redux-toolkit

--- react-tasks/src/components/MainPage/MainPage.tsx
+++ react-tasks/src/components/MainPage/MainPage.tsx
@@ -4,84 +4,49 @@ import Card from 'components/Card/Card';
 import { Loader } from 'components/Loader/Loader';
 import { Modal } from 'components/Modal/Modal';
 import { SearchBar } from 'components/SearchBar/SearchBar';
-import { SearchContext } from 'context/SearchContext';
 
-import React, { useContext, useEffect, useReducer, useState } from 'react';
+import { useAppDispatch, useAppSelector } from 'hooks/redux';
+
+import React, { useEffect } from 'react';
+import { searchSlice } from 'store/reducers/SearchSlice';
 import './MainPage.css';
 
-interface MainPageState {
-  spinnerActive: boolean;
-  modalActive: boolean;
-  searchResults: Array<FlickrSearchItem>;
-  modalContent: FlickrSearchItem | null;
-}
-
-const initialState: MainPageState = {
-  spinnerActive: false,
-  modalActive: false,
-  searchResults: [],
-  modalContent: null,
-};
-
-interface ReducerAction {
-  type: string;
-  payload: boolean;
-}
-
-const reducer = (state: MainPageState, action: ReducerAction) => {
-  const { type, payload } = action;
-  switch (type) {
-    case 'spinnerActive':
-      return { ...state, spinnerActive: payload };
-    case 'modalActive':
-      return { ...state, modalActive: payload };
-    default:
-      return state;
-  }
-};
-
 export function MainPage() {
-  const { results, addResults } = useContext(SearchContext);
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  // const [spinnerActive, setSpinnerActive] = useState(false);
-  // const [modalActive, setModalActive] = useState(false);
-
-  const [searchResults, setSearchResults] = useState<Array<FlickrSearchItem>>([]);
-  const [modalContent, setModalContent] = useState<FlickrSearchItem | null>(null);
+  const { spinnerActive, modalActive, modalContent, searchResults } = useAppSelector(
+    (state) => state.searchReducer
+  );
+  const { toggleSpinner, toggleModal, setCards, setModalContent } = searchSlice.actions;
+  const dispatch = useAppDispatch();
 
   const searchBarCallback = (searchResult: Array<FlickrSearchItem>) => {
-    addResults([...results, ...searchResult]);
-    setSearchResults([...searchResults, ...searchResult]);
+    dispatch(setCards(searchResult));
   };
 
   const ajaxAnimationCallback = (status: boolean) => {
-    //setSpinnerActive(status);
-    dispatch({ type: 'spinnerActive', payload: status });
+    dispatch(toggleSpinner(status));
   };
 
   const modalShowCallback = (status: boolean) => {
-    // setModalActive(status);
-    dispatch({ type: 'modalActive', payload: status });
+    dispatch(toggleModal(status));
   };
 
   const openModal = (index: number) => {
     if (searchResults[index]) {
       modalShowCallback(true);
-      setModalContent(searchResults[index]);
+      dispatch(setModalContent(searchResults[index]));
     }
   };
 
   useEffect(() => {
-    setSearchResults(results);
+    dispatch(setCards(searchResults));
   }, []);
 
   return (
     <>
       <div className="App">
-        {state.spinnerActive && <Loader />}
+        {spinnerActive && <Loader />}
 
-        {state.modalActive && <Modal content={modalContent} onCloseCallback={modalShowCallback} />}
+        {modalActive && <Modal content={modalContent} onCloseCallback={modalShowCallback} />}
 
         <SearchBar
           searchCallBack={searchBarCallback}
---
+++ react-tasks/src/store/reducers/SearchSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { FlickrSearchItem } from 'api/types';
+
+interface SearchState {
+  spinnerActive: boolean;
+  modalActive: boolean;
+  searchResults: Array<FlickrSearchItem>;
+  modalContent: FlickrSearchItem | null;
+}
+
+const initialState: SearchState = {
+  spinnerActive: false,
+  modalActive: false,
+  searchResults: [],
+  modalContent: null,
+};
+
+export const searchSlice = createSlice({
+  name: 'default',
+  initialState,
+  reducers: {
+    toggleSpinner(state, action: PayloadAction<boolean>) {
+      state.spinnerActive = action.payload;
+      console.log(action);
+    },
+    toggleModal(state, action: PayloadAction<boolean>) {
+      state.modalActive = action.payload;
+    },
+    setCards(state, action: PayloadAction<Array<FlickrSearchItem>>) {
+      state.searchResults = [...state.searchResults, ...action.payload];
+    },
+    setModalContent(state, action: PayloadAction<FlickrSearchItem | null>) {
+      state.modalContent = action.payload;
+    },
+  },
+});
+
+export default searchSlice.reducer;
